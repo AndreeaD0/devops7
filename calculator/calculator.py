25b580c9d06d8ea1bf6a64fa3ec1dde3a4dc950a
AndreeaD0 <117246136+AndreeaD0@users.noreply.github.com>
2022-11-23 19:33:45 -0600

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -5,7 +5,7 @@ def add(first_term, second_term):
 
 
 def multiply(first_term, second_term):
-    return first_term * second_term
+    return first_term * second_term + 1 - 1
 
 
 def subtract(first_term, second_term):
