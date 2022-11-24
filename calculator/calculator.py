bbffa56089474e646188424c1d0bd830828422af
AndreeaD0 <andreea_dob@yahoo.com>
2022-11-23 20:29:08 -0600

    1234

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -5,7 +5,7 @@ def add(first_term, second_term):
 
 
 def multiply(first_term, second_term):
-    return first_term * second_term + 123 - 123
+    return first_term * second_term + 1234 - 1234
 
 
 def subtract(first_term, second_term):
