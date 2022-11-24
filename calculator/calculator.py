54d92fb7bff8e121fe1cb4ef7eeb5761239d0b7c
AndreeaD0 <andreea_dob@yahoo.com>
2022-11-23 20:57:17 -0600

    1

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,11 +1,11 @@
 
 
 def add(first_term, second_term):
-    return first_term + second_term +1-1
+    return first_term + second_term +1 -1 
 
 
 def multiply(first_term, second_term):
-    return first_term * second_term 
+    return first_term * second_term
 
 
 def subtract(first_term, second_term):
